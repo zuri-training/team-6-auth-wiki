d2721a033ab340b5c6976989a3b45fd740399049
femmyte <sanyaoluadefemi@gmail.com>
2022-07-25 12:38:20 +0000

    installed tailwindcss dependencies

--- auth_wiki/auth_wiki/settings.py
+++ auth_wiki/auth_wiki/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# tailwindcss theme
+TAILWIND_APP_NAME = 'theme'

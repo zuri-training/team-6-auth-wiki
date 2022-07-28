ee190711bbdad8b4a1e983b02a900d858fc16424
femmyte <sanyaoluadefemi@gmail.com>
2022-07-28 17:16:54 +0000

    Remove a hardcoded Django secret key

--- auth_wiki/auth_wiki/settings.py
+++ auth_wiki/auth_wiki/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tdplkj)a87z!gn&skw(%&lbo8mu6i89x27coux63!rwlqaatqx'
+# SECRET_KEY = 'django-insecure-tdplkj)a87z!gn&skw(%&lbo8mu6i89x27coux63!rwlqaatqx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

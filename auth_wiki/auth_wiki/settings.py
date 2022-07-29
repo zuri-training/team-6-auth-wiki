25fa60d316c072cce66902794005651b357a477d
femmyte <sanyaoluadefemi@gmail.com>
2022-07-29 00:18:49 +0000

    updated the settings.py file

--- auth_wiki/auth_wiki/settings.py
+++ auth_wiki/auth_wiki/settings.py
@@ -12,18 +12,19 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+
+# django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "/static")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-tdplkj)a87z!gn&skw(%&lbo8mu6i89x27coux63!rwlqaatqx'
+SECRET_KEY = 'django-insecure-tdplkj)a87z!gn&skw(%&lbo8mu6i89x27coux63!rwlqaatqx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

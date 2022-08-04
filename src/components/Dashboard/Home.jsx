df764fb0fca8e76946cce4a074b222a212d0dca8
femmyte <sanyaoluadefemi@gmail.com>
2022-08-04 16:30:27 +0000

    make it accessecible to the authenticated user

--- src/components/Dashboard/Home.jsx
+++ src/components/Dashboard/Home.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { BiBell } from 'react-icons/bi'
 import Code from '../Code'
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../../App";
 
 const python = `className AuthInterface:
     def isLoggedIn(self) -> bool:
@@ -20,13 +22,39 @@ const python = `className AuthInterface:
         pass`
 
 const Home = () => {
+  // const { state, dispatch } = useContext(AuthContext);
+
+  // if (!state.isLoggedIn) {
+  //   return <Navigate to="/login" />;
+  // }
+
+  // const { avatar_url, name, public_repos, followers, following } = state.user
+
+  // const handleLogout = () => {
+  //   dispatch({
+  //     type: "LOGOUT"
+  //   });
+  // } 
+
   return (
       <>
         <div className="p-8 col-span-3 bg-[#e9effe]">
     <div className="flex justify-between items-center">
       <h1 className="text-[40px] text-primary">Lorem ipsum</h1>
           <BiBell className="h-6 w-6" />
-    </div>
+        </div>
+        {/* <div className="container">
+        <button onClick={()=> handleLogout()}>Logout</button>
+        <div>
+          <div className="content">
+            <img src={avatar_url} alt="Avatar"/>
+            <span>{name}</span>
+            <span>{public_repos} Repos</span>
+            <span>{followers} Followers</span>
+            <span>{following} Following</span>
+          </div>
+        </div>
+      </div> */}
     <p className="text-[16px] my-7">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim

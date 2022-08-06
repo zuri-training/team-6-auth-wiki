c0c39c24c27bf26628ac4611f9f3ae4605d7c6ef
Adedamola Adeniran <adedamy67@gmail.com>
2022-08-06 14:16:23 +0100

    Updated 'about us' page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};

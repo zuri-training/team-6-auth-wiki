82b1156ddf02b0f094588546eaedfcfc90b9a1cd
femmyte <sanyaoluadefemi@gmail.com>
2022-08-07 02:12:29 +0000

    modified the code component

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -1,7 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Prism from "prismjs";
 import "prismjs/themes/prism-tomorrow.css";
-import '../prism.css'
+import "../prism.css";
+
+import Editor from "react-simple-code-editor";
+
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+
+import "./prism-vsc-dark-pluss.css";
+import "./editor.css";
+
+// export default function Code({codeSnippet }) {
+//   const [code, setCode] = useState(codeSnippet);
+
+//   return (
+//     <div className="App">
+//       <div className='window'>
+//         <div className="title-bar">
+//           <div className="title-buttons">
+//             <div className="title-button"></div>
+//             <div className="title-button"></div>
+//             <div className="title-button"></div>
+//           </div>
+//         </div>
+//         <div className='editor_wrap'>
+//           <Editor
+//             value={code}
+//             onValueChange={code => setCode(code)}
+//             highlight={code => highlight(code, languages.js)}
+//             padding={10}
+//             style={{
+//               fontFamily: '"Fira code", "Fira Mono", monospace',
+//               fontSize: 12
+//             }}
+//           />
+//         </div>
+//       </div>
+
+//     </div>
+//   );
+// }
 
 export default function Code({ code, language }) {
   useEffect(() => {
@@ -10,7 +50,7 @@ export default function Code({ code, language }) {
   return (
     <div className="Code">
       <pre>
-        <code className={`language-${language}`}>{code}</code>
+        <code className={`language-${language} line-numbers`}>{code}</code>
       </pre>
     </div>
   );

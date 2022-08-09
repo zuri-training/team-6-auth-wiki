1e61325eea6474d6f01f939ef4f11c56b04ba583
femmyte <sanyaoluadefemi@gmail.com>
2022-08-09 15:24:20 +0000

    changed the baseUrl to the online URL

--- src/components/api/axios.js
+++ src/components/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://okpanz-cors-anywhere.herokuapp.com/http://myapi.dataxis.ng",
+  baseURL: "https://myapi.dataxis.ng",
 });

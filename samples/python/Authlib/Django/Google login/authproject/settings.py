f3072fd8580b6e35d74d1eae1b02a3bdc18ca57e
XpertHeritage <105379174+HeritageOlayinka@users.noreply.github.com>
2022-08-11 15:51:03 +0100

    Update settings.py
    Update for google login

--- samples/python/Authlib/Django/Google login/authproject/settings.py
+++ samples/python/Authlib/Django/Google login/authproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# AUTHLIB CLIENTS
+AUTHLIB_OAUTH_CLIENTS = {
+    'google': {
+        'client_id': os.getenv('GOOGLE_CLIENT_ID'),
+        'client_secret': os.getenv('GOOGLE_CLIENT_SECRET'),
+    }
+}

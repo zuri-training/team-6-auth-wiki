a3af3a9308297fbbe703406be274dba7c5bdbdd6
femmyte <sanyaoluadefemi@gmail.com>
2022-08-11 12:36:33 +0000

    added a token method

--- src/components/auth/RequireAuth.jsx
+++ src/components/auth/RequireAuth.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import { Navigate, useLocation } from 'react-router-dom'
-import { useAuth } from './auth'
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "./auth";
 
-export const RequireAuth = ({children}) => {
-    const location = useLocation()
-    const auth = useAuth()
-    if (!auth.user) {
-        return <Navigate to='/login' state = {{path: location.pathname}} />
-    }
-    return children
-}
-
+export const RequireAuth = ({ children }) => {
+  const location = useLocation();
+  const auth = useAuth();
+  if (!auth.user) {
+    return <Navigate to="/login" state={{ path: location.pathname }} />;
+  }
+  if (auth.getStorage) {
+    return children;
+  }
+  return children;
+};
